6b26f04f94b9893cf51da378b7a1e4ed58618270
Gabriell <gabriellfertnandess1@gmail.com>
2024-03-12 10:26:11 -0300

    first commit

---
+++ trabalhoP1Brenno/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './views/home/home.component';
+import { EstadoCrudComponent } from './views/estado-crud/estado-crud.component';
+import { CidadeCrudComponent } from './views/cidade-crud/cidade-crud.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'estados',
+    component: EstadoCrudComponent,
+  },
+  {
+    path: 'cidades',
+    component: CidadeCrudComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
